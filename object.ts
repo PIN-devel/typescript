98b76266ddf7ae6aff814a4970193857efc1a995
Innam Park <pin954562@gmail.com>
2022-11-06 14:24:42 +0900

    null

---
+++ object.ts
@@ -0,0 +1,3 @@
+const person1 = { name: "Mark", age: "11" }; // persion1 is not 'object' type. persion1 is '{ name: "Mark", age: "11" }' type.
+const person2 = Object.create({ name: "Mark", age: "11" });
+// primitive type이 아닌 것을 나타내고 싶을 때 사용하는 타입
