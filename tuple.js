98f8038adbb5ca2609be29b35e9a444cb2df1bb8
Innam Park <pin954562@gmail.com>
2022-11-06 14:55:44 +0900

    tuple

---
+++ tuple.js
@@ -0,0 +1,7 @@
+"use strict";
+let x;
+x = ["Mike", 29];
+// x = [10, 'Mike'];
+// x[2] = 'hello'
+const person = ["Mark", 39];
+// const [first, second, third] = person;
---
+++ tuple.ts
@@ -0,0 +1,9 @@
+let x: [string, number];
+
+x = ["Mike", 29];
+// x = [10, 'Mike'];
+// x[2] = 'hello'
+
+const person: [string, number] = ["Mark", 39];
+
+// const [first, second, third] = person;
