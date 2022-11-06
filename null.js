f318481d39d0a61d2e0d655352f2375443d79d4e
Innam Park <pin954562@gmail.com>
2022-11-06 14:19:32 +0900

    null

---
+++ null.js
@@ -0,0 +1,7 @@
+"use strict";
+// let MyName: string = null;
+// let u: undefined = null;
+let v1 = undefined;
+// let v2: void = null;
+let union = null;
+union = "Mark";
---
+++ null.ts
@@ -0,0 +1,7 @@
+// let MyName: string = null;
+// let u: undefined = null;
+let v1: void = undefined;
+// let v2: void = null;
+
+let union: string | null = null;
+union = "Mark";
